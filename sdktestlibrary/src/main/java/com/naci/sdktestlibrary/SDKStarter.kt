f67f5b032d25594a054d740fbe19b8a6f0d58fd0
Naci Özyıldırım <naci.ozyildirim@multinet.com.tr>
2020-09-22 14:45:52 +0300

    Test volley aar

--- sdktestlibrary/src/main/java/com/naci/sdktestlibrary/SDKStarter.kt
+++ sdktestlibrary/src/main/java/com/naci/sdktestlibrary/SDKStarter.kt
@@ -1,10 +1,17 @@
 package com.naci.sdktestlibrary
 
 import android.content.Context
+import android.util.Log
+import com.android.volleyx.Header
+import com.android.volley.Response
+import com.android.volley.VolleyError
+import com.naci.sdktestlibraryconstants.Constants
 
-public class SDKStarter {
+class SDKStarter {
 
     companion object {
+
+        private const val TAG = "SDKStarter"
 
         lateinit var consumeTokenListener: ConsumeTokenListener
 
@@ -13,6 +20,9 @@ public class SDKStarter {
             merchantToken: String?,
             listener: ConsumeTokenListener
         ) {
+            val asd = Header("asd", "asd")
+            val response = Response.error<VolleyError>(VolleyError())
+            Log.d(TAG, "startSDKForSubmitConsumer: END_POINT : ${Constants.END_POINT}")
             consumeTokenListener = listener
             context.startActivity(MainActivity.newInstance(context))
         }
